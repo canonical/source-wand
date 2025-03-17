4ca272bd44d78e0c06a6be108103ed392d67c29c
Thinking-Dragon <clement.gassmann-prince@canonical.com>
2025-03-17 18:11:20 -0400

    Implement dependency tree generator for Python

--- dependency-analysis/src/dependency_tree_generators/mod.rs
+++ dependency-analysis/src/dependency_tree_generators/mod.rs
@@ -1,3 +1,4 @@
+use python_pip_dependency_tree_generator::generate_python_pip_dependency_tree;
 use source_wand_common::project_manipulator::project_manipulator::AnyProjectManipulator;
 use rust_cargo_dependency_tree_generator::generate_rust_cargo_dependency_tree;
 
@@ -7,11 +8,18 @@ use crate::{
 };
 
 pub mod rust_cargo_dependency_tree_generator;
+pub mod python_pip_dependency_tree_generator;
 
-pub fn generate_dependency_tree(build_system: BuildSystemIdentity, project_manipulator: &AnyProjectManipulator) -> Result<DependencyTreeNode, String> {
+pub fn generate_dependency_tree(
+    build_system: BuildSystemIdentity,
+    project_manipulator: &AnyProjectManipulator
+) -> Result<DependencyTreeNode, String> {
     match build_system {
         BuildSystemIdentity::RustCargo => {
             generate_rust_cargo_dependency_tree(project_manipulator)
+        },
+        BuildSystemIdentity::PythonPip => {
+            generate_python_pip_dependency_tree(project_manipulator)
         },
     }
 }
