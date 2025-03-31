2b81e77b657518b50c3f45848a6ccdc32071615d
Thinking-Dragon <clement.gassmann-prince@canonical.com>
2025-03-31 16:42:41 -0400

    Implement java/maven dependency tree parser

--- dependency-analysis/src/dependency_tree_generators/java_maven_dependency_tree_generator.rs
+++ dependency-analysis/src/dependency_tree_generators/java_maven_dependency_tree_generator.rs
@@ -1,109 +1,114 @@
+use regex::Regex;
 use source_wand_common::{project::Project, project_manipulator::project_manipulator::{AnyProjectManipulator, ProjectManipulator}};
 use crate::dependency_tree_node::DependencyTreeNode;
 
-#[derive(Debug)]
-struct DepthAnnotatedNode {
-    node: DependencyTreeNode,
-    depth: u32,
-}
-
 pub fn generate_java_maven_dependency_tree(project_manipulator: &AnyProjectManipulator) -> Result<DependencyTreeNode, String> {
-    let raw_tree: String = project_manipulator.run_shell(
+    let logs: String = project_manipulator.run_shell(
         "mvn dependency:tree".to_string()
     )?;
 
-    let mut parsed_nodes = Vec::new();
-    let root_project = parse_root_project(project_manipulator)?;
+    let (root, branches) = extract_tree_from_logs(&logs)?;
 
-    parsed_nodes.push(DepthAnnotatedNode {
-        depth: 0,
-        node: DependencyTreeNode::new(root_project.clone(), Vec::new()),
-    });
+    let mut anotated_tree: Vec<DepthAnnotatedDependencyTreeNode> = anotate_tree(&root, &branches);
 
-    for line in raw_tree.lines() {
-        if line.is_empty() ||
-           line.starts_with("[INFO] ---") ||
-           line.starts_with("[INFO] Finished at: ") ||
-           line.starts_with("Building ")
-        {
-            continue;
-        }
+    for i in (1..anotated_tree.len()).rev() {
+        let depth: u32 = anotated_tree[i].depth;
 
-        if line.contains(&format!("{}:{}", root_project.name, root_project.version)) {
-            continue;
-        }
-
-        if let Ok((depth, dependency)) = parse_dependency_line(line) {
-            parsed_nodes.push(DepthAnnotatedNode {
-                depth,
-                node: DependencyTreeNode::new(dependency, Vec::new()),
-            });
-        }
-    }
-
-    for i in (1..parsed_nodes.len()).rev() {
-        let current_depth = parsed_nodes[i].depth;
-        let mut parent_index = i - 1;
-
-        while parent_index > 0 && parsed_nodes[parent_index].depth >= current_depth {
+        let mut parent_index: usize = i - 1;
+        while parent_index > 0 && anotated_tree[parent_index].depth >= depth {
             parent_index -= 1;
         }
 
-        let node = parsed_nodes[i].node.clone();
-        parsed_nodes[parent_index].node.dependencies.push(Box::new(node));
+        let node: DependencyTreeNode = anotated_tree[i].node.clone();
+        anotated_tree[parent_index].node.dependencies.push(Box::new(node));
     }
 
-    Ok(parsed_nodes[0].node.clone())
+    Ok(anotated_tree[0].node.clone())
 }
 
-fn parse_root_project(project_manipulator: &AnyProjectManipulator) -> Result<Project, String> {
-    let artifact_id = project_manipulator.run_shell(
-        "mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout".to_string()
-    )?.trim().to_string();
-
-    let version = project_manipulator.run_shell(
-        "mvn help:evaluate -Dexpression=project.version -q -DforceStdout".to_string()
-    )?.trim().to_string();
-
-    Ok(Project::new(
-        artifact_id,
-        version,
-        String::new(),
-        String::new(),
-    ))
+#[derive(Debug)]
+struct DepthAnnotatedDependencyTreeNode {
+    pub node: DependencyTreeNode,
+    pub depth: u32,
 }
 
-fn parse_dependency_line(line: &str) -> Result<(u32, Project), String> {
-    let clean_line = line.trim_start_matches("[INFO] ");
-    let mut depth = 0;
-    let mut chars = clean_line.chars().peekable();
+fn anotate_tree(root: &String, branches: &Vec<String>) -> Vec<DepthAnnotatedDependencyTreeNode> {
+    let mut nodes: Vec<DepthAnnotatedDependencyTreeNode> = Vec::new();
 
-    while let Some(c) = chars.peek() {
-        match c {
-            ' ' | '│' | '├' | '└' | '─' | '+' | '\\' => {
-                depth += 1;
-                chars.next();
+    nodes.push(DepthAnnotatedDependencyTreeNode {
+        node: DependencyTreeNode::new(parse_project(root), Vec::new()),
+        depth: 0,
+    });
+
+    for branch in branches {
+        let (branch, depth) = compute_branch_depth(branch);
+
+        if branch.is_empty() {
+            continue;
+        }
+
+        let node: DependencyTreeNode = DependencyTreeNode::new(parse_project(&branch), Vec::new());
+        nodes.push(DepthAnnotatedDependencyTreeNode { node, depth });
+    }
+
+    nodes
+}
+
+fn parse_project(raw: &String) -> Project {
+    let tokens: Vec<String> = raw.split(":").map(|token| token.to_string()).collect();
+    let name: String = format!("{}.{}", tokens.get(0).unwrap(), tokens.get(1).unwrap());
+    let version: String = tokens.get(3).unwrap().to_string();
+    let license: String = String::new();
+    let repository: String = String::new();
+
+    Project::new(name, version, license, repository)
+}
+
+fn compute_branch_depth(branch: &String) -> (String, u32) {
+    let initial_length: usize = branch.len();
+
+    let branch_prefix_expr: Regex = Regex::new(r"^[\| +\\-]+").unwrap();
+    let striped_branch: String = branch_prefix_expr.replace(&branch, String::new()).into_owned();
+
+    let final_length: usize = striped_branch.len();
+    let depth: u32 = (initial_length - final_length) as u32;
+
+    (striped_branch, depth)
+}
+
+fn extract_tree_from_logs(logs: &String) -> Result<(String, Vec<String>), String> {
+    let record_begin_marker: &str = "[\u{1b}[1;34mINFO\u{1b}[m] \u{1b}[1m--- ";
+    let record_end_marker: &str = "[\u{1b}[1;34mINFO\u{1b}[m] \u{1b}[1m-";
+    let prefix_to_strip: &str = "[\u{1b}[1;34mINFO\u{1b}[m] ";
+
+    let mut lines: Vec<String> = Vec::new();
+    let mut record: bool = false;
+
+    for line in logs.lines() {
+        if line.starts_with(record_end_marker) {
+            record = false;
+        }
+
+        if record {
+            if let Some(line) = line.strip_prefix(prefix_to_strip) {
+                lines.push(line.to_string());
             }
-            _ => break,
+        }
+
+        if line.starts_with(record_begin_marker) {
+            record = true;
         }
     }
 
-    let depth = (depth / 3) as u32;
+    let (root, branches) = lines.split_first()
+        .ok_or("Invalid tree format. Could not separate root from branches.")?;
 
-    let dep_str: String = chars.collect();
-    let parts: Vec<&str> = dep_str.split(':').collect();
-
-    if parts.len() < 4 {
-        return Err(format!("Invalid dependency format: {}", dep_str));
-    }
-
-    let artifact_id = parts[1].trim().to_string();
-    let version = parts[3].trim().to_string();
-
-    Ok((depth, Project::new(
-        artifact_id,
-        version,
-        String::new(),
-        String::new(),
-    )))
+    Ok(
+        (
+            root.to_string(),
+            branches.iter()
+                    .map(|branch| branch.to_string())
+                    .collect()
+        )
+    )
 }
