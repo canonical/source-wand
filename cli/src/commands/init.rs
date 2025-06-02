b7961acf5ac6f5f974081a9ee2327da98b511595
atandrewlee <personal@andrewleeofficial.com>
2025-06-02 09:08:12 -0400

    added local & byname to init command; still broken but will be fixed

--- cli/src/commands/init.rs
+++ cli/src/commands/init.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use anyhow::{Error, Result};
 use clap::Parser;
 use source_wand_common::utils::write_yaml_file::write_yaml_file;
@@ -10,24 +12,76 @@ use source_wand_onboarding::plan::plan_onboarding::plan_onboarding;
 
 #[derive(Debug, Parser)]
 pub struct InitArgs {
-    #[arg(long)]
-    from_git: String,
-
-    #[arg(long)]
-    checkout: Option<String>,
+    #[command(subcommand)]
+    command: InitCommand,
 }
+
+#[derive(Debug, Parser)]
+pub enum InitCommand {
+    #[command(about = "From a local project.")]
+    Local(LocalDependenciesArgs),
+    #[command(about = "From a project in a git repository.")]
+    Git(GitDependenciesArgs),
+    #[command(about = "From the name/version pair of a project.")]
+    ByName(NameDependenciesArgs),
+}
+
+#[derive(Debug, Parser)]
+pub struct LocalDependenciesArgs {
+    path: PathBuf,
+}
+
+#[derive(Debug, Parser)]
+pub struct GitDependenciesArgs {
+    url: String,
+    branch: Option<String>,
+}
+
+#[derive(Debug, Parser)]
+pub struct NameDependenciesArgs {
+    name: String,
+    version: String,
+}
+
 
 pub fn init_command(args: &InitArgs) -> Result<()> {
     println!("Planning project onboarding");
     println!(" > Generating dependency tree");
 
+    let dependency_tree = match &args.command {
+        InitCommand::Local(args) => {
+            find_dependency_tree(
+                DependencyTreeRequest::LocalProject {
+                    path: args.path.clone()
+                }
+            ).map_err(|e| Error::msg(e))?
+        }
+        InitCommand::Git(args) => {
+            find_dependency_tree(
+                DependencyTreeRequest::GitProject {
+                    url: args.url.clone(),
+                    branch: args.branch.clone(),
+                }
+            ).map_err(|e| Error::msg(e))?
+        }
+        InitCommand::ByName(args) => {
+            find_dependency_tree(
+                DependencyTreeRequest::NameBased {
+                    name: args.name.clone(),
+                    version: args.version.clone(),
+                }
+            ).map_err(|e|Error::msg(e))?
+        }
+    };
+
+    /*
     let dependency_tree: DependencyTreeNode = find_dependency_tree(
         DependencyTreeRequest::GitProject {
             url: args.from_git.clone(),
             branch: args.checkout.clone(),
         }
     ).map_err(|e| Error::msg(e))?;
-
+    */
     let nb_dependencies: usize = dependency_tree.flatten().dependencies.len();
     println!(" > {} unique dependencies were found in the project", nb_dependencies);
     println!(" > Saving dependency tree");
