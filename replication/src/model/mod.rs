cd70fd2f7b63a0a21d84a5f2ae71f7eb552a7149
Thinking-Dragon <clement.gassmann-prince@canonical.com>
2025-07-26 16:28:31 +0200

    Add list of dependencies to packages in replication manifest

--- replication/src/model/mod.rs
+++ replication/src/model/mod.rs
@@ -1,7 +1,13 @@
 pub mod replication;
+
 pub mod hooks;
+
 pub mod package;
+
 pub mod package_origin;
 pub mod package_origin_git;
+
 pub mod package_destination;
 pub mod package_destination_git;
+
+pub mod dependency;
--- replication/src/model/package.rs
+++ replication/src/model/package.rs
@@ -1,10 +1,11 @@
 use serde::{Serialize, Deserialize};
 
-use crate::model::{package_destination::PackageDestination, package_origin::PackageOrigin};
+use crate::model::{dependency::Dependency, package_destination::PackageDestination, package_origin::PackageOrigin};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Package {
     pub level: u32,
     pub origin: PackageOrigin,
     pub destination: PackageDestination,
+    pub dependencies: Vec<Dependency>,
 }
