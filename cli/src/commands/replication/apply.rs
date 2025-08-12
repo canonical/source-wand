0803cd8a98a96680f94fe5d9ca3b36391f0985f4
Thinking-Dragon <clement.gassmann-prince@canonical.com>
2025-08-12 11:01:09 -0400

    Use UUIDs for package directories

--- cli/src/commands/replication/apply.rs
+++ cli/src/commands/replication/apply.rs
@@ -1,11 +1,19 @@
-use std::{fs::create_dir_all, path::PathBuf};
+use std::{fs::{create_dir_all, remove_dir_all}, path::PathBuf};
 
 use anyhow::Result;
 use clap::Parser;
 use colorize::AnsiColor;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use source_wand_common::{project_manipulator::{local_project_manipulator::LocalProjectManipulator, project_manipulator::ProjectManipulator}, utils::read_yaml_file::read_yaml_file};
+use source_wand_common::{
+    project_manipulator::{
+        local_project_manipulator::LocalProjectManipulator,
+        project_manipulator::ProjectManipulator
+    }
+};
 use source_wand_replication::model::{package::Package, package_destination::PackageDestination, package_origin::PackageOrigin, replication_plan::ReplicationPlan};
+use uuid::Uuid;
+
+use crate::commands::replication::plan::plan_replication;
 
 #[derive(Debug, Parser)]
 pub struct ReplicationApplyArgs;
@@ -13,7 +21,10 @@ pub struct ReplicationApplyArgs;
 fn replicate_package(package: Package) -> Result<()> {
     if let PackageOrigin::GoCache(origin) = package.origin {
         let PackageDestination::Git(destination) = package.destination;
-        let dependency_directory: PathBuf = PathBuf::from(format!("./source-wand/dependencies/{}/{}", origin.name, origin.version));
+
+        let uuid: Uuid = Uuid::new_v4();
+        let dependency_directory: PathBuf = PathBuf::from(format!("./source-wand/{}", uuid));
+
         create_dir_all(&dependency_directory)?;
 
         let sh: LocalProjectManipulator = LocalProjectManipulator::new(dependency_directory, true);
@@ -34,18 +45,23 @@ fn replicate_package(package: Package) -> Result<()> {
         sh.run_shell(format!("git push -u origin {}", destination.reference))?;
 
         println!("{}", format!("[replicated] {} ({})", origin.name, origin.version).green());
+
+        sh.cleanup();
     }
+
     Ok(())
 }
 
 pub fn replicate_apply_command(_args: &ReplicationApplyArgs) -> Result<()> {
-    let replication_plan: ReplicationPlan = read_yaml_file("source-wand/replication-plan.yaml")?;
-    
+    let replication_plan: ReplicationPlan = plan_replication()?;
+
     let results: Vec<Result<()>> = replication_plan
         .packages
         .into_par_iter()
         .map(replicate_package)
         .collect();
+
+    remove_dir_all(PathBuf::from("./source-wand")).ok();
 
     for result in results {
         result?;
