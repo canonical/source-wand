fd8d4b9734debe35b4aac4de841e736a0c1b2532
atandrewlee <andrew.lee@canonical.com>
2025-08-29 11:13:28 -0400

    multi with go_dep

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, sync::{Arc, Mutex}};
 
 use anyhow::Result;
 use clap::{Parser, Subcommand};
@@ -42,9 +42,10 @@ fn main() {
     let version: String = "v1.2.0".to_string();
     let project_root: PathBuf = PathBuf::from("/home/andrew/source-wand-projects");
     let module_name: String = "github.com/canonical/chisel".to_string();
-    let mut graph: Graph<DependencyTreeNodeGo, String> = Graph::new();
-    parse_dependency(&url, &version, &project_root, &module_name, &mut graph); 
-    graph.print_dependencies();
+    let graph = Arc::new(Mutex::new(Graph::new()));
+    let graph_clone = Arc::clone(&graph);
+    parse_dependency(&url, &version, &project_root, &module_name, graph_clone); 
+    graph.lock().unwrap().print_dependencies();
     //println!("{:#?}", graph);
     //println!("{}", graph.to_dot());
 }
